95f2c54d3b8e9e31b8b0ebce48a67efc96acf272
mijatovicmile <mile@milemijatovic.com>
2019-11-18 23:22:10 +0100

    Animation states update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
-import { trigger, state, style } from '@angular/animations';
+import { trigger, 
+         state, 
+         style, 
+         transition, 
+         animate 
+        } from '@angular/animations';
 
 @Component({
   selector: 'my-app',
@@ -9,16 +14,49 @@ import { trigger, state, style } from '@angular/animations';
     trigger('divState', [
       state('normal', style({
         'background-color': 'blue',
+        transform: 'translateX(0) scale(1)'
+      })),
+      state('highlighted', style({
+        'background-color': 'red',
+        transform: 'translateX(100px) scale(1)'
+      })),
+      state('shrinken', style({
+        'background-color': 'green',
+        transform: 'translateX(0px) scale(0.5)'
+      })),
+      transition('normal <=> highlighted', animate(300)),
+      // transition('highlighted => normal', animate(600))
+    ]),
+    trigger('wildState', [
+      state('normal', style({
+        'background-color': 'blue',
         transform: 'translateX(0)'
       })),
       state('highlighted', style({
         'background-color': 'red',
         transform: 'translateX(100px)'
-      }))
+      })),
+      state('shrinken', style({
+        'background-color': 'green',
+        transform: 'translateX(0px) scale(0.5)'
+      })),
+      transition('normal => highlighted', animate(300)),
+      transition('highlighted => normal', animate(800)),
+      transition('shrinken <=> *', animate(500))
     ])
   ]
 })
 export class AppComponent  {
   state = 'normal';
-  name = 'Angular';
+  wildState = 'normal';
+
+  onAnimate() {
+    this.state == 'normal' ? this.state = 'highlighted' : this.state = 'normal';
+
+    this.wildState == 'normal' ? this.wildState = 'shrinken' : this.wildState = 'normal';
+  }
+
+  onShrink() {
+    this.wildState = 'shrinken'
+  }
 }
